20b6958f60fb1a5741dbc409375cac34a950c675
nguyenmaixuanhuong <xuanhuong22211@gmail.com>
2024-03-09 09:28:50 +0700

    Update login, register

--- Frontend/src/components/FormAccount/index.jsx
+++ Frontend/src/components/FormAccount/index.jsx
@@ -6,6 +6,9 @@ import Modal from '@mui/material/Modal';
 import TextField from '@mui/material/TextField';
 import HighlightOffIcon from '@mui/icons-material/HighlightOff';
 import './FormAccount.styles.scss';
+import validator from 'validator';
+import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
+import CountdownTimer from './CountTime';
 const style = {
     position: 'absolute',
     top: '50%',
@@ -18,12 +21,63 @@ const style = {
 };
 function Register(props) {
     const [open, setOpen] = React.useState(false);
+    const [openModalConfirm, setOpenModalConfirm] = React.useState(false);
     const [email, setEmail] = React.useState('');
-    const handleOpen = () => setOpen(true);
-    const handleClose = () => setOpen(false);
-    const changeEmail = React.useCallback((event) => {
+    const [error, setError] = React.useState(false);
+    const [number, setNumber] = React.useState('');
+    const [timeLeft, setTimeLeft] = React.useState(10);
+
+    const handleOpen = () => {
+        setOpen(true)
+    };
+    const handleClose = () => {
+        setEmail('')
+        setOpen(false)};
+
+    const handleCloseConfirm = () =>{
+        setEmail('')
+        setOpenModalConfirm(false);
+    } 
+        
+
+    const handleOpenConfirm = () => setOpenModalConfirm(true);
+
+    const changeEmail = (event) => {
+        setError(false)
         setEmail(event.target.value);
-    }, []);
+    }
+
+
+    const changeNumber = (event) => {
+        setNumber(event.target.value);
+    }
+
+    const backPrevious = () => {
+        setEmail(email);
+        handleOpen();
+        setOpenModalConfirm(false);
+    }
+    const stepContinue = () => {
+        setOpen(false);
+        handleOpenConfirm();
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault()
+        if (validator.isEmail(email)) {
+            setError(false);
+            stepContinue();
+        }
+        else {
+            setError(true)
+        }
+
+    }
+
+    const getTimer = React.useCallback((timeLeft) => {
+        setTimeLeft(timeLeft);
+    }, [])
+
     return (
         <div>
             {props.name === false ? (
@@ -58,15 +112,62 @@ function Register(props) {
                             label="Email"
                             value={email}
                             onChange={changeEmail}
+                            error={!!error}
+                            helperText={error ? 'Email không hợp lệ' : ''}
                         />
                         {props.name === false ?
-                            <Button variant="contained" sx={{ mt: 2, ml: 15, width: 300, textAlign: 'center', padding: 1 }}>ĐĂNG KÝ</Button>
+                            <Button onClick={handleSubmit} variant="contained" sx={{ mt: 2, ml: 15, width: 300, textAlign: 'center', padding: 1 }}>ĐĂNG KÝ</Button>
                             :
-                            <Button variant="contained" sx={{ mt: 2, ml: 15, width: 300, textAlign: 'center', padding: 1 }}>ĐĂNG NHẬP</Button>
+                            <Button onClick={handleSubmit} variant="contained" sx={{ mt: 2, ml: 15, width: 300, textAlign: 'center', padding: 1 }}>ĐĂNG NHẬP</Button>
                         }
                     </div>
                 </Box>
             </Modal>
+
+            <div className="modal-confirm">
+                <div>
+                    <Modal
+                        open={openModalConfirm}
+                        onClose={handleCloseConfirm}
+                        aria-labelledby="modal-modal-title"
+                        aria-describedby="modal-modal-description"
+                    >
+                        <Box sx={style}>
+                            <div className='confirm-title'>
+                                <ArrowBackIosNewIcon className='confirm-iconback' onClick={backPrevious} fontSize='small'></ArrowBackIosNewIcon>
+                                <Typography className='confirm-text' id="modal-modal-title" variant="h6" component="h2">
+                                    NHẬP MÃ XÁC NHẬN
+                                </Typography>
+                                <HighlightOffIcon className='confirm-close' onClick={handleCloseConfirm}></HighlightOffIcon>
+                            </div>
+                            <div className='register-body'>
+
+                                <TextField
+                                    fullWidth
+                                    className='input-number'
+                                    required
+                                    id="outlined-required"
+                                    label="Mã xác nhận"
+                                    value={number}
+                                    onChange={changeNumber}
+                                    error={error}
+                                    helperText={error ? 'number không hợp lệ' : ''}
+                                />
+                                <Typography id="modal-modal-description" sx={{ mt: 2, textAlign: 'center', fontStyle: 'italic' }}>
+                                    Mã xác nhận đã được gửi đến email của bạn.
+                                </Typography>
+                                <div >
+                                    <CountdownTimer initialTime={timeLeft} getTimer={getTimer} /> 
+                                </div>
+                                {/* <Typography id="modal-modal-description" sx={{ textAlign: 'center', color: 'red' }}>
+                                    Nếu không nhận được mã.
+                                </Typography> */}
+                                <Button variant="contained" sx={{ mt: 2, ml: 15, width: 300, textAlign: 'center', padding: 1 }}>XÁC NHẬN</Button>
+                            </div>
+                        </Box>
+                    </Modal>
+                </div>
+            </div>
         </div>
     );
 }
